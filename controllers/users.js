7e5ac68b3fa87774a58479ca9a74de4de1f3c64b
Weston Bailey <void@MBP.local>
2020-07-03 23:35:45 -0700

    added some views

--- controllers/users.js
+++ controllers/users.js
@@ -6,12 +6,12 @@ const isLoggedIn = require('../middleware/isLoggedIn');
 const flash = require('connect-flash');
 const passport = require('../config/ppConfig');
 
-//todo change to /users/:index/profile
+
 router.get('/', /*isLoggedIn,*/ function(req, res){
   res.send('render all users')
 });
 
-//todo change to /users/:index/profile
+
 router.get('/:userId', /*isLoggedIn,*/ function(req, res){
   userId = req.params.userId;
   res.send(`profile for ${userId}`);
